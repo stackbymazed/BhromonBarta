847ea2ca1cfa219580f6e3f0e869e5d82ece76c1
Nayem783982 <majedulislam223311@gmail.com>
2025-07-14 20:46:53 +0600

    sent the firebase accesstoken server side

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
-import React from 'react';
+import React, { use } from 'react';
+import { AuthContext } from '../Contexts/AuthContext/AuthContext';
 
 const axiosSecure = axios.create({
-    baseURL:import.meta.env.VITE_SERVER_URL
+    baseURL: import.meta.env.VITE_SERVER_URL
 })
 const useAxiosSecure = () => {
+    const { user } = use(AuthContext)
+    axiosSecure.interceptors.request.use(config => {
+        config.headers.Authorization = `Bearer ${user.accessToken}`
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    })
     return axiosSecure
 };
 
